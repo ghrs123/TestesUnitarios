fc7c735a6bb066a835a910b3e3e08cca5fa4dfd6
Gustavo Santos <g_henrique19@hotmail.com>
2022-07-29 22:13:40 +0100

    Revert "Revert "update fetures""
    This reverts commit f4cefbc56a20504e60650a9bea0abf0d48e67004.

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -14,6 +14,7 @@ import org.junit.rules.ErrorCollector;
 import org.junit.rules.ExpectedException;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -45,9 +46,12 @@ public class LocacaoServiceTest {
 	public void testeLocacao() throws Exception {
 		//cenario
 		Usuario usuario1 = new Usuario("Pedro Marques");
-		Filme filme = new  Filme("Filme 1",5,5.0);
 
-		listFilme.add(filme);
+		Filme filme1 = new  Filme("Filme 1",5,5.0);
+		Filme filme2 = new  Filme("Filme 12",10,5.0);
+		Filme filme3 = new  Filme("Filme 3",10,5.0);
+
+		listFilme = Arrays.asList(filme1,filme2,filme3);
 		//ação
 		Locacao locacao = service.alugarFilme(usuario1, listFilme);
 
@@ -61,7 +65,7 @@ public class LocacaoServiceTest {
 //		assertThat(isMesmaData(locacao.getDataLocacao(), new Date()),CoreMatchers.is(true));
 //		assertThat(isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), CoreMatchers.not(true));
 
-		error.checkThat(locacao.getValor(), is(equalTo(5.0)));
+		error.checkThat(locacao.getValor(), is(equalTo(15.0)));
 		error.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
 		error.checkThat(isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), not(false));
 
